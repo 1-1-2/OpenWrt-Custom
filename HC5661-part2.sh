fb139087266ba71b25f4791b328fbee7b4ca5ffb
1-1-2 <50569812+1-1-2@users.noreply.github.com>
2022-06-22 11:43:21 +0800

    small flash ch modulize

--- HC5661-part2.sh
+++ HC5661-part2.sh
@@ -20,7 +20,7 @@ sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_gener
 # sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
 echo '修改机器名称'
-sed -i 's/OpenWrt/GEE_1S/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Gee_1s/g' package/base-files/files/bin/config_generate
 
 target_inf() {
     #=========================================
@@ -83,12 +83,26 @@ CONFIG_PACKAGE_jq=y
 CONFIG_PACKAGE_coreutils-base64=y
 # ----------Kernel modules-USB Support-kmod-usb3
 CONFIG_DEFAULT_kmod-usb3=y
+# ----------luci-app-hd-idle
+CONFIG_PACKAGE_luci-app-hd-idle=y
 # ----------luci-app-cifsd
 CONFIG_PACKAGE_luci-app-cifsd=y
 # ----------luci-app-commands
 CONFIG_PACKAGE_luci-app-commands=y
+# ----------luci-app-qos
+CONFIG_PACKAGE_luci-app-qos=m
+# ----------luci-app-eqos
+CONFIG_PACKAGE_luci-app-eqos=m
+# ----------luci-app-sqm
+CONFIG_PACKAGE_luci-app-sqm=m
 # ----------luci-app-ttyd
 CONFIG_PACKAGE_luci-app-ttyd=y
+# ----------luci-app-wrtbwmon
+CONFIG_PACKAGE_luci-app-wrtbwmon=m
+# ----------luci-theme-argon
+CONFIG_PACKAGE_luci-theme-bootstrap=y
+CONFIG_PACKAGE_luci-theme-argonne=m
+CONFIG_PACKAGE_luci-app-argonne-config=m
 # ----------luci-app-webadmin
 CONFIG_PACKAGE_luci-app-webadmin=y
 EOF
@@ -100,12 +114,23 @@ config_func() {
     # 功能包
     #=========================================
     cat >> .config << EOF
+# ----------luci-app-aria2
+CONFIG_PACKAGE_luci-app-aria2=m
 # ----------luci-app-VPNs
-CONFIG_PACKAGE_luci-app-nps=y
+CONFIG_PACKAGE_luci-app-nps=m
+CONFIG_PACKAGE_luci-app-frpc=m
+CONFIG_PACKAGE_luci-app-n2n_v2=m
+CONFIG_PACKAGE_luci-app-zerotier=m
 # ----------luci-app-openclash
-CONFIG_PACKAGE_luci-app-openclash=y
+CONFIG_PACKAGE_luci-app-openclash=m
+# ----------network-firewall-ip6tables-ip6tables-mod-nat
+# CONFIG_PACKAGE_ip6tables-mod-nat=y
+# ----------luci-app-transmission
+CONFIG_PACKAGE_luci-app-transmission=m
 # ----------luci-app-watchcat
-# CONFIG_PACKAGE_luci-app-watchcat=y
+CONFIG_PACKAGE_luci-app-watchcat=y
+# ----------luci-app-v2ray-server
+CONFIG_PACKAGE_luci-app-v2ray-server=m
 EOF
 }
 
@@ -115,10 +140,7 @@ config_test() {
     # 测试域
     #=========================================
     cat >> .config << EOF
-# ----------luci-theme-argon
-CONFIG_PACKAGE_luci-theme-bootstrap=y
-CONFIG_PACKAGE_luci-theme-argonne=y
-CONFIG_PACKAGE_luci-app-argonne-config=y
+CONFIG_PACKAGE_luci-app-verysync=m
 EOF
 }
 
