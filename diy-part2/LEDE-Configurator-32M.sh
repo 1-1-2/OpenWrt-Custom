596a55060a43da6092b7bf9a7a81428961ce7dad
1-1-2 <50569812+1-1-2@users.noreply.github.com>
2022-07-05 21:52:33 +0800

    Merge from https://github.com/1-1-2/Actions-Debug/tree/79a9e44e45b5e219db77d2b721bb2edd8d8714ef
    区分 LEDE 和 OpenWrt 源

---
+++ diy-part2/LEDE-Configurator-32M.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+#
+# Copyright (c) 2022-now 1-1-2 <https://github.com/1-1-2>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# File name: OpenWrt-Configurator-32M.sh
+# Description: OpenWrt .config maker script (for addon&paks) for 32MB(256Mb) flash device
+#
+
+cat << EOF
+=======OpenWrt-Configurator-32M.sh=======
+	functions loaded:
+		1. config_func
+		2. config_basic
+		3. config_clean
+		4. config_test
+=========================================
+EOF
+
+config_clean() {
+    #=========================================
+    # Stripping options
+    #=========================================
+    cat >> .config << EOF
+CONFIG_STRIP_KERNEL_EXPORTS=y
+# CONFIG_USE_MKLIBS=y
+EOF
+    #=========================================
+    # Remove defaults Apps
+    #=========================================
+    cat >> .config << EOF
+# ----------luci-app-ssr-plus
+# CONFIG_PACKAGE_luci-app-ssr-plus is not set
+EOF
+}
+
+config_basic() {
+    config_clean
+    #=========================================
+    # 基础包和应用
+    #=========================================
+    cat >> .config << EOF
+# ----------extra packages-automount
+CONFIG_PACKAGE_automount=y
+# ----------extra packages-ipv6helper
+CONFIG_PACKAGE_ipv6helper=y
+# ----------Utilities-Disc-cfdisk&fdisk
+CONFIG_PACKAGE_cfdisk=y
+CONFIG_PACKAGE_fdisk=y
+# ----------Utilities-Filesystem-e2fsprogs
+CONFIG_PACKAGE_e2fsprogs=y
+# ----------Utilities-usbutils
+CONFIG_PACKAGE_usbutils=y
+# ----------Utilities-jq
+CONFIG_PACKAGE_jq=y
+# ----------Utilities-coreutils-base64
+CONFIG_PACKAGE_coreutils-base64=y
+# ----------Kernel modules-USB Support-kmod-usb3
+CONFIG_DEFAULT_kmod-usb3=y
+# ----------luci-app-hd-idle
+CONFIG_PACKAGE_luci-app-hd-idle=y
+# ----------luci-app-cifsd
+CONFIG_PACKAGE_luci-app-cifsd=y
+# ----------luci-app-commands
+CONFIG_PACKAGE_luci-app-commands=y
+# ----------luci-app-qos
+CONFIG_PACKAGE_luci-app-qos=y
+# ----------luci-app-eqos
+CONFIG_PACKAGE_luci-app-eqos=y
+# ----------luci-app-sqm
+CONFIG_PACKAGE_luci-app-sqm=y
+# ----------luci-app-ttyd
+CONFIG_PACKAGE_luci-app-ttyd=y
+# ----------luci-app-wrtbwmon
+CONFIG_PACKAGE_luci-app-wrtbwmon=y
+# ----------luci-theme-argon
+CONFIG_PACKAGE_luci-theme-bootstrap=y
+CONFIG_PACKAGE_luci-theme-argonne=y
+CONFIG_PACKAGE_luci-app-argonne-config=y
+# ----------luci-app-webadmin
+CONFIG_PACKAGE_luci-app-webadmin=y
+EOF
+}
+
+config_func() {
+    config_basic
+    #=========================================
+    # 功能包
+    #=========================================
+    cat >> .config << EOF
+# ----------luci-app-aria2
+CONFIG_PACKAGE_luci-app-aria2=y
+# ----------luci-app-VPNs
+CONFIG_PACKAGE_luci-app-nps=y
+CONFIG_PACKAGE_luci-app-frpc=y
+CONFIG_PACKAGE_luci-app-n2n_v2=y
+CONFIG_PACKAGE_luci-app-zerotier=y
+# ----------luci-app-openclash
+CONFIG_PACKAGE_luci-app-openclash=y
+# ----------network-firewall-ip6tables-ip6tables-mod-nat
+# CONFIG_PACKAGE_ip6tables-mod-nat=y
+# ----------luci-app-transmission
+CONFIG_PACKAGE_luci-app-transmission=y
+# ----------luci-app-watchcat
+CONFIG_PACKAGE_luci-app-watchcat=y
+# ----------luci-app-v2ray-server
+CONFIG_PACKAGE_luci-app-v2ray-server=y
+EOF
+}
+
+config_test() {
+    config_func
+    #=========================================
+    # 测试域
+    #=========================================
+    cat >> .config << EOF
+# CONFIG_PACKAGE_luci-app-verysync=y
+EOF
+}
---
+++ diy-part2/OpenWrt-Configurator-32M.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+#
+# Copyright (c) 2022-now 1-1-2 <https://github.com/1-1-2>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# File name: OpenWrt-Configurator-32M.sh
+# Description: OpenWrt .config maker script (for addon&paks) for 32MB(256Mb) flash device
+#
+
+cat << EOF
+=======OpenWrt-Configurator-32M.sh=======
+	functions loaded:
+		1. add_packages
+		2. config_func
+		3. config_basic
+		4. config_clean
+		5. config_test
+=========================================
+EOF
+
+add_packages(){
+	#=========================================
+	# 两种方式（没有本质上的区别）：
+	# M1. 从别的(类)OpenWrt源码仓库部分借用，放到feeds文件夹(通常为feeds/luci)
+	# M2. 拉取专门的luci包到package文件夹
+	#=========================================
+	
+	# M1
+	echo '从 lean 那里借个 luci-app-vsftpd'
+	svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-vsftpd feeds/luci/applications/luci-app-vsftpd
+	echo '还有依赖 vsftpd-alt'
+	svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/vsftpd-alt package/lean/vsftpd-alt
+	./scripts/feeds update -i luci
+	./scripts/feeds install -p luci luci-app-vsftpd
+
+	exist_sed(){
+		if [ -f "$1" ]; then
+			cp "$1" tmp/before_mod.bak
+			sed -i 's/services/nas/' "$1"
+			echo "将$(basename "$1" | cut -d. -f1)从services移动到了nas"
+			diff before_mod.bak "$1"
+			echo '---EOF---'
+		else
+			echo 没找到$1
+		fi
+	}
+	echo 'luci-app-vsftpd定义了一级菜单<nas>，顺便修改一些菜单入口到该菜单'
+	exist_sed feeds/luci/applications/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json
+	exist_sed feeds/luci/applications/luci-app-hd-idle/root/usr/share/luci/menu.d/luci-app-hd-idle.json
+	exist_sed feeds/luci/applications/luci-app-aria2/root/usr/share/luci/menu.d/luci-app-aria2.json
+	exist_sed feeds/luci/applications/luci-app-transmission/root/usr/share/luci/menu.d/luci-app-transmission.json
+
+	# M2
+	cd package
+
+	echo '从 Hyy2001X 那里借一个改好的 luci-app-npc'
+	svn co https://github.com/Hyy2001X/AutoBuild-Packages/trunk/luci-app-npc
+	# git clone https://github.com/lloyd18/luci-app-npc
+	git clone https://github.com/lloyd18/npc
+
+	echo '从 lean 那里借一个自动外存挂载 automount'
+	svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/automount lean/automount
+	sed -i 's/ +ntfs3-mount//' lean/automount/Makefile      # 去掉不存在的包
+
+	cd ..
+
+    # 解决无法正确识别出简体中文语言包的问题
+    # ref: https://github.com/ysc3839/luci-proto-minieap/pull/2
+    find -type d -path '*/po/zh-cn' | xargs dirname | xargs -I'{}' ln -fs {}/zh-cn {}/zh_Hans
+}
+
+config_clean() {
+    #=========================================
+    # Stripping options
+    #=========================================
+    cat >> .config << EOF
+CONFIG_STRIP_KERNEL_EXPORTS=y
+# CONFIG_USE_MKLIBS=y
+EOF
+    #=========================================
+    # Luci
+    #=========================================
+    cat >> .config << EOF
+CONFIG_PACKAGE_luci=y
+CONFIG_LUCI_LANG_zh_Hans=y
+EOF
+}
+
+config_basic() {
+    config_clean
+    #=========================================
+    # 基础包和应用
+    #=========================================
+    cat >> .config << EOF
+# ----------select for openwrt
+CONFIG_PACKAGE_luci-app-acl=y
+CONFIG_PACKAGE_luci-app-advanced=y
+CONFIG_PACKAGE_luci-app-ddns=y
+CONFIG_PACKAGE_luci-app-statistics=y
+CONFIG_PACKAGE_luci-app-nlbwmon=y
+CONFIG_PACKAGE_luci-app-store=y
+CONFIG_PACKAGE_luci-app-upnp=y
+CONFIG_PACKAGE_luci-app-wol=y
+# ----------automount from lean
+CONFIG_PACKAGE_automount=y
+CONFIG_PACKAGE_block-mount=y
+CONFIG_PACKAGE_kmod-fs-ext4=y
+CONFIG_PACKAGE_kmod-fs-exfat=y
+CONFIG_PACKAGE_kmod-fs-ntfs=y
+# ----------Utilities-usbutils
+CONFIG_PACKAGE_usbutils=y
+# ----------Kernel modules-USB Support-kmod-usb3
+CONFIG_DEFAULT_kmod-usb3=y
+# ----------Utilities-Disc-cfdisk&fdisk
+CONFIG_PACKAGE_cfdisk=y
+CONFIG_PACKAGE_fdisk=y
+# ----------Utilities-Filesystem-e2fsprogs
+CONFIG_PACKAGE_e2fsprogs=y
+# ----------luci-app-hd-idle
+CONFIG_PACKAGE_luci-app-hd-idle=y
+# ----------Utilities-jq
+CONFIG_PACKAGE_jq=y
+# ----------Utilities-coreutils-base64
+CONFIG_PACKAGE_coreutils-base64=y
+# ----------luci-app-ksmbd
+CONFIG_PACKAGE_luci-app-ksmbd=y
+# ----------luci-app-commands
+CONFIG_PACKAGE_luci-app-commands=y
+# ----------luci-app-qos
+CONFIG_PACKAGE_luci-app-qos=y
+# ----------luci-app-eqos
+CONFIG_PACKAGE_luci-app-eqos=y
+# ----------luci-app-sqm
+CONFIG_PACKAGE_luci-app-sqm=y
+# ----------luci-app-ttyd
+CONFIG_PACKAGE_luci-app-ttyd=y
+# ----------luci-theme-argon
+CONFIG_PACKAGE_luci-theme-bootstrap=y
+CONFIG_PACKAGE_luci-theme-argon=y
+CONFIG_PACKAGE_luci-app-argon-config=y
+EOF
+}
+
+config_func() {
+    config_basic
+    #=========================================
+    # 功能包
+    #=========================================
+    cat >> .config << EOF
+# ----------luci-app-vsftpd
+CONFIG_PACKAGE_luci-app-vsftpd=y
+# ----------luci-app-aria2
+CONFIG_PACKAGE_luci-app-aria2=y
+# ----------luci-app-VPNs
+CONFIG_PACKAGE_luci-app-npc=y
+CONFIG_PACKAGE_luci-app-frpc=y
+# ----------luci-app-openclash
+CONFIG_PACKAGE_luci-app-openclash=y
+# CONFIG_PACKAGE_dnsmasq is not set
+# ----------network-firewall-ip6tables-ip6tables-mod-nat
+# CONFIG_PACKAGE_ip6tables-mod-nat=y
+# ----------luci-app-transmission
+CONFIG_PACKAGE_luci-app-transmission=y
+# ----------luci-app-watchcat
+CONFIG_PACKAGE_luci-app-watchcat=y
+EOF
+}
+
+config_test() {
+    config_func
+    #=========================================
+    # 测试域
+    #=========================================
+    cat >> .config << EOF
+# CONFIG_PACKAGE_luci-app-verysync=y
+EOF
+}
