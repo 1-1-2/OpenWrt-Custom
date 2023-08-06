697dbd44f6cf3e4a93302c49becbc7795fbb2e8f
1-1-2 <50569812+1-1-2@users.noreply.github.com>
2023-08-06 18:45:34 +0800

    功能和脚本调整
    1. 梳理 add_packages 和 modification 的逻辑
    2. 增加拉取 luci-app-n2n
    3. 调整了各配置的内容，有增有减

--- diy-part2/Configurator-OpenWrt-32M.sh
+++ diy-part2/Configurator-OpenWrt-32M.sh
@@ -22,6 +22,11 @@ EOF
 
 modification() {
     # 一些可能必要的修改
+
+    echo '[FIX]创建硬链接，解决无法正确识别出简体中文语言包的问题'
+    # ref: https://github.com/ysc3839/luci-proto-minieap/pull/2
+    find -type d -path '*/po/zh-cn' | xargs dirname | xargs -I'{}' ln -srvn {}/zh-cn {}/zh_Hans
+
     echo '[MOD]更换 luci-app-clash 的依赖 openssl 为 mbedtls'
     find -type f -path '*/luci-app-clash/Makefile' -print -exec sed -i 's/openssl/mbedtls/w /dev/stdout' {} \;
 
@@ -49,11 +54,35 @@ modification() {
     # svn co https://github.com/coolsnowwolf/packages/trunk/lang/node feeds/packages/lang/node
 
     echo '[FIX]PKG_USE_MIPS16已被openwrt主线弃用，修改外部包的 PKG_USE_MIPS16:=0 为 PKG_BUILD_FLAGS:=no-mips16'
-    find -type f -name Makefile | xargs -i sh -c 'echo -n "[{}]\t"; sed -i "/PKG_USE_MIPS16:=0/{
-s//PKG_BUILD_FLAGS:=no-mips16/g
-w /dev/stdout
-}" {}'
+    find -type f -name Makefile -exec sh -c '
+        if grep -q "PKG_USE_MIPS16:=0" "$1"; then
+            echo -n "[$1] "
+            sed -i "s/PKG_USE_MIPS16:=0/PKG_BUILD_FLAGS:=no-mips16/w /dev/stdout" "$1"
+        fi
+    ' sh {} \;
 
+    echo 'luci-app-vsftpd 定义了一级菜单 <nas>，顺便修改一些菜单入口到该菜单'
+    exist_sed() {
+        if [ -f "$1" ]; then
+            cp -f "$1" tmp/exist_sed.before
+            sed -i 's/services/nas/' "$1"
+            echo "将 $(basename "$1" | cut -d. -f1) 从 services 移动到 nas" [$1]
+            diff tmp/exist_sed.before "$1"
+            echo "=====================EOdiff======================="
+        else
+            echo 没找到$1
+        fi
+    }
+    exist_sed feeds/luci/applications/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json
+    exist_sed feeds/luci/applications/luci-app-hd-idle/root/usr/share/luci/menu.d/luci-app-hd-idle.json
+    exist_sed feeds/luci/applications/luci-app-aria2/root/usr/share/luci/menu.d/luci-app-aria2.json
+    exist_sed feeds/luci/applications/luci-app-transmission/root/usr/share/luci/menu.d/luci-app-transmission.json
+
+    echo 'luci-app-n2n 定义了一级菜单 <VPN>，把 nps 也搬进去'
+    sed -i 's/services/vpn/w /dev/stdout' feeds/luci/applications/luci-app-nps/luasrc/controller/nps.lua
+
+    echo '把 luci-app-nft-qos 从 <services> 搬到 <network>'
+    sed -i 's/services/network/w /dev/stdout' feeds/luci/applications/luci-app-nft-qos/luasrc/controller/nft-qos.lua
 }
 
 add_packages(){
@@ -76,27 +105,13 @@ add_packages(){
     svn co https://github.com/coolsnowwolf/packages/trunk/multimedia/UnblockNeteaseMusic feeds/packages/multimedia/UnblockNeteaseMusic
     svn co https://github.com/coolsnowwolf/packages/trunk/multimedia/UnblockNeteaseMusic-Go feeds/packages/multimedia/UnblockNeteaseMusic-Go
 
-    echo '从天灵那里借个 luci-app-nps'
+    echo '从天灵那里借个 luci-app-nps 和 luci-app-n2n'
     svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-nps feeds/luci/applications/luci-app-nps
-    echo '还有依赖 nps'
+    svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-n2n feeds/luci/applications/luci-app-n2n
+    echo '还有依赖 nps 和 n2n'
     svn co https://github.com/immortalwrt/packages/trunk/net/nps feeds/packages/net/nps
+    svn co https://github.com/immortalwrt/packages/trunk/net/n2n feeds/packages/net/n2n
 
-    exist_sed(){
-        if [ -f "$1" ]; then
-            cp -f "$1" tmp/exist_sed.before
-            sed -i 's/services/nas/' "$1"
-            echo "将 $(basename "$1" | cut -d. -f1) 从 services 移动到 nas" [$1]
-            diff tmp/exist_sed.before "$1"
-            echo "=====================EOdiff======================="
-        else
-            echo 没找到$1
-        fi
-    }
-    echo 'luci-app-vsftpd 定义了一级菜单 <nas>，顺便修改一些菜单入口到该菜单'
-    exist_sed feeds/luci/applications/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json
-    exist_sed feeds/luci/applications/luci-app-hd-idle/root/usr/share/luci/menu.d/luci-app-hd-idle.json
-    exist_sed feeds/luci/applications/luci-app-aria2/root/usr/share/luci/menu.d/luci-app-aria2.json
-    exist_sed feeds/luci/applications/luci-app-transmission/root/usr/share/luci/menu.d/luci-app-transmission.json
 
     # M2
     cd package
@@ -108,17 +123,15 @@ add_packages(){
     svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/automount lean/automount
     sed -i 's/ +ntfs3-mount//w /dev/stdout' lean/automount/Makefile      # 去掉不存在的包
 
-    cd ..
+    echo '从 lisaac 那里加载 luci-app-diskman'
     mkdir -p package/luci-app-diskman && \
-    wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/applications/luci-app-diskman/Makefile -O package/luci-app-diskman/Makefile
+    wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/applications/luci-app-diskman/Makefile -O luci-app-diskman/Makefile
     mkdir -p package/parted && \
-    wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
+    wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O parted/Makefile
 
-    # 解决无法正确识别出简体中文语言包的问题
-    # ref: https://github.com/ysc3839/luci-proto-minieap/pull/2
-    find -type d -path '*/po/zh-cn' | xargs dirname | xargs -I'{}' ln -srvn {}/zh-cn {}/zh_Hans
+    cd ..
 
-    # 修改一些依赖
+    # 修正依赖，调整菜单
     modification
     # 最后[强制]更新一下索引和安装一下包
     ./scripts/feeds update -ifa
@@ -167,14 +180,25 @@ config_basic() {
     #=========================================
     cat >> .config << EOF
 # ----------select for openwrt
+CONFIG_PACKAGE_ddns-scripts-cloudflare=y
 CONFIG_PACKAGE_luci-app-acl=y
 CONFIG_PACKAGE_luci-app-advanced=y
 CONFIG_PACKAGE_luci-app-ddns=y
+CONFIG_PACKAGE_luci-app-ledtrig-rssi=y
+CONFIG_PACKAGE_luci-app-ledtrig-switch=y
+CONFIG_PACKAGE_luci-app-ledtrig-usbport=y
 CONFIG_PACKAGE_luci-app-statistics=y
-CONFIG_PACKAGE_luci-app-nlbwmon=y
 CONFIG_PACKAGE_luci-app-store=y
 CONFIG_PACKAGE_luci-app-upnp=y
 CONFIG_PACKAGE_luci-app-wol=y
+CONFIG_PACKAGE_luci-proto-wireguard=y
+CONFIG_PACKAGE_which=y
+# ----------Utilities-network
+CONFIG_PACKAGE_bind-dig=y
+CONFIG_PACKAGE_bind-host=y
+CONFIG_PACKAGE_ca-certificates=y
+CONFIG_PACKAGE_ethtool=y
+CONFIG_PACKAGE_socat=y
 # ----------automount from lean
 CONFIG_PACKAGE_automount=y
 CONFIG_PACKAGE_block-mount=y
@@ -204,16 +228,16 @@ CONFIG_PACKAGE_luci-app-commands=y
 CONFIG_PACKAGE_luci-app-qos=y
 # ----------luci-app-nft-qos
 CONFIG_PACKAGE_luci-app-nft-qos=y
-# ----------luci-app-eqos
-CONFIG_PACKAGE_luci-app-eqos=y
 # ----------luci-app-sqm
 CONFIG_PACKAGE_luci-app-sqm=y
 # ----------luci-app-ttyd
 CONFIG_PACKAGE_luci-app-ttyd=y
+# ----------luci-app-uhttpd
+CONFIG_PACKAGE_luci-app-uhttpd=y
 # ----------luci-theme-argon
-CONFIG_PACKAGE_luci-theme-bootstrap=y
-CONFIG_PACKAGE_luci-theme-argon=y
 CONFIG_PACKAGE_luci-app-argon-config=y
+CONFIG_PACKAGE_luci-theme-argon=y
+CONFIG_PACKAGE_luci-theme-bootstrap=y
 EOF
 }
 
@@ -228,14 +252,16 @@ CONFIG_PACKAGE_luci-app-vsftpd=y
 # ----------luci-app-aria2
 CONFIG_PACKAGE_luci-app-aria2=y
 # ----------luci-app-VPNs
+CONFIG_PACKAGE_luci-app-n2n=y
 CONFIG_PACKAGE_luci-app-nps=y
-CONFIG_PACKAGE_luci-app-frpc=y
 # ----------luci-app-transmission
 CONFIG_PACKAGE_luci-app-transmission=y
 # ----------luci-app-watchcat
 CONFIG_PACKAGE_luci-app-watchcat=y
-# ----------iperf3
+# ----------Utilities
 CONFIG_PACKAGE_iperf3=y
+CONFIG_PACKAGE_kcptun-client=y
+CONFIG_PACKAGE_tcpdump-mini=y
 EOF
 }
 
@@ -250,14 +276,14 @@ CONFIG_PACKAGE_luci-app-openclash=y
 # ----------network-firewall-ip6tables-ip6tables-mod-nat
 # CONFIG_PACKAGE_ip6tables-mod-nat=y
 
-CONFIG_PACKAGE_luci-app-ledtrig-rssi=y
-CONFIG_PACKAGE_luci-app-ledtrig-switch=y
-CONFIG_PACKAGE_luci-app-ledtrig-usbport=y
-CONFIG_PACKAGE_luci-app-uhttpd=y
-CONFIG_PACKAGE_luci-app-unblockmusic=y
 CONFIG_PACKAGE_luci-app-adblock=y
-CONFIG_PACKAGE_luci-app-tinyproxy=y
-CONFIG_PACKAGE_luci-app-wireguard=y
-CONFIG_PACKAGE_tcpdump-mini=y
+CONFIG_PACKAGE_luci-app-diskman=y
+CONFIG_PACKAGE_luci-app-frpc=y
+# CONFIG_PACKAGE_luci-app-nlbwmon=y
+# CONFIG_PACKAGE_luci-app-tinyproxy=y
+
+CONFIG_PACKAGE_luci-app-unblockmusic=y
+# CONFIG_PACKAGE_luci-app-unblockmusic_INCLUDE_UnblockNeteaseMusic_Go=y
+# CONFIG_PACKAGE_luci-app-unblockmusic_INCLUDE_UnblockNeteaseMusic_NodeJS is not set
 EOF
 }
