815aef9e373b7ee2475f16f1c1c96a1ce24e5701
1-1-2 <50569812+1-1-2@users.noreply.github.com>
2025-04-03 12:17:28 +0800

    先拿at91试试水

--- device-env.sh
+++ device-env.sh
@@ -63,7 +63,14 @@ case $1 in
         SEQ_FILE="testSeq/openwrt.ini"
         ;;
     7)
-        # undefined
+        DEVICE_TAG="OpenWrt - WS1408(WIP)"
+        REPO_USE=openwrt/openwrt
+        REPO_BRANCH=master
+#         COMMIT_SHA=latest
+        DEVICE_ARCH="arm"
+        DIY_P2_SH="diy-part2/[OpenWrt]WS1408-part2.sh"
+        DEPENDS=$(curl -fsSL "https://gist.githubusercontent.com/1-1-2/38e424cd9da729f72fa4a495d23271ea/raw/openwrt")
+        SEQ_FILE="testSeq/openwrt.ini"
         ;;
     *)
         echo "input error"
